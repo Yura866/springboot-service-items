993041f63a924d743d58a86736004dac836804b1
Yura866 <yuriy.guzio@gmail.com>
2020-05-02 15:33:17 +0200

    changed itemcontroller

--- src/main/java/com/springboot/app/item/controller/ItemController.java
+++ src/main/java/com/springboot/app/item/controller/ItemController.java
@@ -1,9 +1,16 @@
 package com.springboot.app.item.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,10 +22,14 @@ import com.springboot.app.item.service.ItemService;
 
 @RestController
 public class ItemController {
+	private static Logger log = LoggerFactory.getLogger(ItemController.class);
 
 	@Autowired
 	@Qualifier("serviceFeign")
 	ItemService itemService;
+	
+	@Value("${configuracion.texto}")
+	private String text;
 	
 	@GetMapping("/list")
 	public List<Item> list(){		
@@ -41,6 +52,16 @@ public class ItemController {
 		item.setProduct(product);
 		
 		return item;
+	}
+	
+	@GetMapping("/retriev-config")
+	public ResponseEntity<?> retrievConfig(@Value("${server.port}") String port){
+		log.info(text);
+		Map<String, String> json  = new HashMap<>();
+		json.put("text","text");
+		json.put("port",port);
+		
+		return new ResponseEntity<Map<String,String>>(json,HttpStatus.OK);
 	}
 	
 }
